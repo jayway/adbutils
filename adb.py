224a02e24d2f7d277a14d1d60f3d6ae2ee4d3841
Andreas Nilsson <andreas.nilsson@jayway.com>
2013-01-29 14:47:44 +0100

    Added error capturing and logging.

--- adb.py
+++ adb.py
@@ -1,9 +1,26 @@
+import logging
 import  subprocess
 
 __author__    = 'Andreas Nilsson'
 __email__     = 'andreas.nilsson@jayway.com'
 __copyright__ = "Copyright 2013, Jayway"
 __license__   = 'Apache 2.0'
+
+#Logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+#Formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+#Setup Console logging
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+
+#Add logger handlers
+logger.addHandler(ch)
+
 
 def run_instrument(package, instrument_runner='android.test.InstrumentationTestRunner', class_name=None):
     '''
@@ -18,5 +35,12 @@ def run_instrument(package, instrument_runner='android.test.InstrumentationTestR
 
     cmd = r'adb shell "am instrument -w %(clazz)s %(package)s/%(test_runner)s"' %\
           {'package': package,'test_runner': instrument_runner, 'clazz': class_str, }
-    #        print cmd
-    return subprocess.check_output(cmd, shell=True)
+
+    logger.debug("Running cmd: %s", cmd)
+
+    try:
+        return subprocess.check_output(cmd, shell=True)
+    except subprocess.CalledProcessError as e:
+        logger.error('args: %s; return code: %s; cmd: %s; message: %s', e.args, e.returncode, e.cmd, e.message)
+        return ''
+
